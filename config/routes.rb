ee50507a68fb5ff6ad44b52799e31655a34788a8
tykowale <tykowale@gmail.com>
2015-12-13 20:48:24 -0600

    add user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   devise_for :users
-    namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-        scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-            resources :users, :only => [:show]
-        end
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, :only => [:show, :create]
     end
+  end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,19 +1,55 @@
 require 'spec_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-    before(:each) { request.headers['Accept'] = 'applications/vnd.marketplace.v1'}
+  before(:each) { request.headers['Accept'] = 'applications/vnd.marketplace.v1'}
 
-    describe "GET #show" do
-        before(:each) do
-            @user = FactoryGirl.create :user
-            get :show, id:@user.id, format: :json
-        end
-
-        it "returns the information about a reporter on a hash" do
-            user_response = JSON.parse(response.body, symbolize_names: true)
-            expect(user_response[:email]).to eql @user.email
-        end
-
-        it { should respond_with 200 }
+  describe "GET #show" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :show, id:@user.id, format: :json
     end
+
+    it "returns the information about a reporter on a hash" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql @user.email
+    end
+
+    it { should respond_with 200 }
+  end
+
+
+  describe "POST #create" do
+    context "when successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when not created" do
+      before(:each) do
+        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "gives an explanation" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
