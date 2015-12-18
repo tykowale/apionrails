e7e8ee3c128073caaa30327c0501cf72f1ef7bfd
tykowale <tykowale@gmail.com>
2015-12-17 21:12:35 -0600

    add destroy session method

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -35,4 +35,13 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
     end
   end
 
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
+  end
 end
